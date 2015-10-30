42f684cb49491c6d049b999420534ea4ccbedbb4
hellflame <hellflamedly@gmail.com>
2015-10-31 01:27:40 +0800

    仓库移动

--- setup.py
+++ setup.py
@@ -7,24 +7,33 @@ __author__ = 'hellflame'
 
 setup(
     name='qiniuManager',
-    version='0.8.0',
+    version='0.9.0',
     keywords=('qiniu', 'console manager', 'qiniu upload'),
     description="终端管理七牛云空间",
     license="MIT",
     author=__author__,
     author_email='hellflamedly@gmail.com',
     url='https://github.com/hellflame/qiniu',
-    packages=find_packages(),
+    packages=[
+        'qiniu_manager'
+    ],
+    install_requires=[
+        'qiniu'
+    ],
     platforms="linux, mac os",
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        "Environment :: Console",
+        "Operating System :: MacOS",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
         'Programming Language :: Python :: 2.7'
     ],
     entry_points={
         'console_scripts': [
-            'qiniu=qiniu_manager.manager:'
+            'qiniu=qiniu_manager.manager:main'
         ]
     }
 )
