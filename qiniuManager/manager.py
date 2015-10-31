3e6816caf6362c5a5f6cb4bbb954b1bf2e1c8adf
hellflame <hellflamedly@gmail.com>
2015-10-31 18:53:10 +0800

    修改帮助菜单输出格式

--- qiniuManager/manager.py
+++ qiniuManager/manager.py
@@ -193,13 +193,13 @@ def main():
 
     if len(argv) <= 1 or '--help' in argv or '-h' in argv:
         print '\n*********帮助模式*********\n'
-        deploy_ex = "\tdeploy like this\n\t\t" \
-                    "qiniu your.file\n\t\t" \
-                    "qiniu --access 'your access key'\n\t\t" \
-                    "qiniu --secret 'your secret key'\n\t\tqiniu --space 'space name'\n"
+        deploy_ex = "  deploy like this\n\t" \
+                    "qiniu your.file\n\t" \
+                    "qiniu --access 'your access key'\n\t" \
+                    "qiniu --secret 'your secret key'\n\tqiniu --space 'space name'\n"
         print(deploy_ex)
         for i in map_desc:
-            print '\t' + i + ' ===> ' + map_desc[i]
+            print '  ' + i + '     ' + map_desc[i]
         exit(0)
     qiniu = Qiniu()
     if '--access' in argv:
