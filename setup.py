a33551641751da47121992b7f14d55764f32cb15
hellflame <hellflamedly@gmail.com>
2015-11-01 12:09:30 +0800

    更新说明

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __author__ = 'hellflame'
 
 setup(
     name='qiniuManager',
-    version='0.9.13',
+    version='0.9.14',
     keywords=('qiniu', 'qiniu console manager', 'qiniu upload'),
     description="终端管理七牛云空间",
     license="MIT",
