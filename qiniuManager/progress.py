e4310bae7d035d85e795f192b6db589957db8513
hellflame <hellflamedly@gmail.com>
2017-10-02 14:38:46 +0800

    更新chunked进度条

--- qiniuManager/progress.py
+++ qiniuManager/progress.py
@@ -3,10 +3,11 @@
     Progress Bar decorator for classes
 """
 import sys
-import subprocess
 import math
 import functools
 
+from subprocess import check_output
+from itertools import cycle
 from qiniuManager.utils import str_len
 
 __all__ = ['bar']
@@ -24,16 +25,7 @@ def bar(width=0, fill='#'):
             if not hasattr(self, 'progressed') or not hasattr(self, 'total'):
                 print("progressed, total attribute is needed!")
                 return
-
-            if not width:
-                try:
-                    w = int(subprocess.check_output("stty size", stderr=None, shell=True).split(b" ")[1])
-                except Exception as e:
-                    print(e)
-                    w = 50
-            else:
-                w = width
-
+            progress_cursor = 1
             while self.progressed <= self.total:
                 func(self, *args, **kwargs)
                 if not hasattr(self, 'disable_progress') or not self.disable_progress:
@@ -41,27 +33,46 @@ def bar(width=0, fill='#'):
                         print("Total Length Invalid !")
                         self.progressed = self.total = 1
                         break
+                    if not width:
+                        try:
+                            w = int(check_output("stty size", stderr=None, shell=True).split(b" ")[1])
+                        except:
+                            w = 50
+                    else:
+                        w = width
+                    if not hasattr(self, 'chunked') or not self.chunked:
+                        percent = self.progressed / float(self.total)
+                        # marks count
+                        percent_show = "{}%".format(int(percent * 100))
+                        # marks width
+                        title = getattr(self, 'title', '')
+                        mark_width = w - len(percent_show) - str_len(title) - 7
+                        mark_count = int(math.floor(mark_width * percent))
+                        sys.stdout.write(
+                            ' ' + title + ' ' +
+                            '[' + fill * mark_count + ' ' * (mark_width - mark_count) + ']  ' + percent_show + '\r')
+                    else:
+                        progress_cursor += 1
+                        title = getattr(self, 'title', '')
+                        mark_width = w - str_len(title) - 6
+                        sys.stdout.write(" " + title + " " +
+                                         "[" +
+                                         "".join([i for _, i in zip(range(mark_width),
+                                                                    cycle([">> ", " >>", "> >"][progress_cursor % 3]))])
+                                         + "] \r")
 
-                    percent = self.progressed / float(self.total)
-                    # marks count
-                    percent_show = "{}%".format(int(percent * 100))
-                    # marks width
-                    title = getattr(self, 'title', '')
-                    mark_width = w - len(percent_show) - 5 - str_len(title) - 2
-                    mark_count = int(math.floor(mark_width * percent))
-                    sys.stdout.write(
-                        ' ' + title + ' ' +
-                        '[' + fill * mark_count + ' ' * (mark_width - mark_count) + ']  ' + percent_show + '\r')
                     sys.stdout.flush()
+
                     if self.progressed == self.total:
                         sys.stdout.write(" " * w + '\r')
-                        sys.stdout.flush()
                         break
                 else:
                     if self.progressed == self.total:
                         break
         return arguments
     return function_wrapper
+
+
 
 
 
--- qiniuManager/test/crypto_test.py
+++ qiniuManager/test/crypto_test.py
@@ -1,3 +1,4 @@
+# coding=utf8
 import unittest
 import random
 import string
